11b760e639eafa78cef533f0a99b268798ef55d1
Samson Lasode <samflicks@gmail.com>
2019-07-05 12:42:59 +0100

    add middleware to /get 'v1/item/:admin_id

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 import express from 'express';
 import data from './data/data';
-const PORT =3000;
+import { runInNewContext } from 'vm';
+const PORT = 3000;
 
-const app=express();
+const app = express();
 
 app.use(express.static('public'));
-app.use('/images',express.static('images'));
+app.use('/images', express.static('images'));
 // publish all data
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
   res.json(data);
 });
 //publish specific data
-app.get('/v1/item/:admin_id',(req, res)=>{
+app.get('/v1/item/:admin_id', (req, res, next) => {
   console.log(req.params.admin_id);
-let user=Number(req.params.admin_id);
+  let user = Number(req.params.admin_id);
 
-res.send(data[user]);
-});
-app.post('/newItem',(req,res)=>{
+  res.send(data[user]);
+  next();
+},
+  (res, req) => {
+    console.log('Did you get the right data?');
+  }
+);
+app.post('/newItem', (req, res) => {
   res.send(`Handler for post on route /newItem`);
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(` This app is listening on PORT:${PORT}`);
-  
+
 })

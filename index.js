55bbcafaf8296b51b11b183ba90c8541d0e36be8
Samson Lasode <samflicks@gmail.com>
2019-07-05 12:20:11 +0100

    create project structure, install dependencies,create express server routes

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import data from './data/data';
+const PORT =3000;
+
+const app=express();
+
+app.use(express.static('public'));
+app.use('/images',express.static('images'));
+// publish all data
+app.get('/',(req, res)=>{
+  res.json(data);
+});
+//publish specific data
+app.get('/v1/item/:admin_id',(req, res)=>{
+  console.log(req.params.admin_id);
+let user=Number(req.params.admin_id);
+
+res.send(data[user]);
+});
+app.post('/newItem',(req,res)=>{
+  res.send(`Handler for post on route /newItem`);
+});
+
+app.listen(PORT, ()=>{
+  console.log(` This app is listening on PORT:${PORT}`);
+  
+})
